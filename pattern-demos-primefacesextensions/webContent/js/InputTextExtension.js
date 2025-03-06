67de8f8fcd1572af5c57444e15cb68310e7de240
Rastislav Vocko <138106833+rastislavvocko@users.noreply.github.com>
2025-03-06 22:12:00 +0100

    TG-2381 Added comments to primefaces extensions

--- pattern-demos-primefacesextensions/webContent/js/InputTextExtension.js
+++ pattern-demos-primefacesextensions/webContent/js/InputTextExtension.js
@@ -1,30 +1,39 @@
+//This is example how to extend existing method init.
 PrimeFaces.widget.InputText.prototype.init = function(cfg) {
-        this.cfg = cfg;
-		this.jq = $(PrimeFaces.escapeClientId(this.cfg.id));
-        PrimeFaces.skinInput(this.jq);
+	this.cfg = cfg;
+	this.jq = $(PrimeFaces.escapeClientId(this.cfg.id));
+    PrimeFaces.skinInput(this.jq);
 
-        //Counter
-        if(this.cfg.counter) {
-            this.counter = this.cfg.counter ? $(PrimeFaces.escapeClientId(this.cfg.counter)) : null;
-            this.cfg.counterTemplate = this.cfg.counterTemplate||'{0}';
-            this.updateCounter();
+     if(this.cfg.counter) {
+     	this.counter = this.cfg.counter ? $(PrimeFaces.escapeClientId(this.cfg.counter)) : null;
+        this.cfg.counterTemplate = this.cfg.counterTemplate||'{0}';
+        this.updateCounter();
 
-            if(this.counter) {
-                var $this = this;
-                this.jq.on('input.inputtext-counter', function(e) {
-                    $this.updateCounter();
-                });
-            }
+        if(this.counter) {
+            var $this = this;
+            this.jq.on('input.inputtext-counter', function(e) {
+                $this.updateCounter();
+            });
+           }
         }
 	
-		 this.normalizeNewlines(this.jq.val());
-		 var $this = this;
-         this.jq.on('keyup.inputtextarea-maxlength', function(e) {
-         	if(e.currentTarget.maxLength > 0){
-            while(byteSize(e.currentTarget.value) > e.currentTarget.maxLength) {
-            e.currentTarget.value = e.currentTarget.value.slice(0,-1);
+	// In the following lines, we are extending the existing functionality of InputText to check the maximum length not in characters but in bytes,
+	// because some characters, such as umlauts, take more than one byte.
+	this.normalizeNewlines(this.jq.val());
+	var $this = this;
+    this.jq.on('keyup.inputtextarea-maxlength', function(e) {
+        if(e.currentTarget.maxLength > 0){
+           	while(byteSize(e.currentTarget.value) > e.currentTarget.maxLength) {
+           		e.currentTarget.value = e.currentTarget.value.slice(0,-1);
+				this.changeColor();
+        		}
             }
-             }
-        });
-       
-    }
+     });
+}
+
+//This is example how to add new method to widget.
+PrimeFaces.widget.InputText = PrimeFaces.widget.InputText.extend({
+	changeColor: function() {
+		
+	},
+});
--- pattern-demos-primefacesextensions/webContent/js/functions.js
+++ pattern-demos-primefacesextensions/webContent/js/functions.js
@@ -1,1 +1,2 @@
+//Method for counting byte size used in InputTextExtension.js.
 const byteSize = str => new Blob([str]).size;
