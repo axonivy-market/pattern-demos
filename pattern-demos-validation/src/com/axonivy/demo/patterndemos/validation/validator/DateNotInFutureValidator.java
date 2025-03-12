cca50b9c46c8e1a563031a3a2c935f1b4af7483c
Filip Baloun <filip.baloun@axonivy.com>
2025-03-12 16:39:20 +0100

    Validation - intermediate commit of ServerSideValidation pattern

---
+++ pattern-demos-validation/src/com/axonivy/demo/patterndemos/validation/validator/DateNotInFutureValidator.java
@@ -0,0 +1,30 @@
+package com.axonivy.demo.patterndemos.validation.validator;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+import ch.ivyteam.ivy.environment.Ivy;
+
+@FacesValidator("dateNotInFutureValidator")
+public class DateNotInFutureValidator implements Validator {
+	public DateNotInFutureValidator() {
+	}
+
+	@Override
+	public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+		var dateValue = (LocalDate) value;
+		var label = (String) component.getAttributes().get("label");
+		if (!dateValue.isAfter(LocalDate.now())) {
+			var errorMessage = Ivy.cms().co("/Validation/dateNotInFuture", Arrays.asList(label));
+			throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, errorMessage, errorMessage));
+		}
+	}
+}
+
