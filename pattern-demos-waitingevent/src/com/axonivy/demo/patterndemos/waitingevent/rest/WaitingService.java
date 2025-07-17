bff5ce5893be1035fbd25c84bc1ec118f8ceed20
nbthong <thong.nguyenba@axonactive.com>
2025-07-17 11:07:13 +0700

    MARP-2397: Add "Waiting Steps" demo to market Pattern Demos

---
+++ pattern-demos-waitingevent/src/com/axonivy/demo/patterndemos/waitingevent/rest/WaitingService.java
@@ -0,0 +1,34 @@
+package com.axonivy.demo.patterndemos.waitingevent.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+import com.axonivy.demo.patterndemos.waitingevent.utils.IvyUtils;
+
+/**
+ * REST service for handling "waiting" events in the system.
+ * 
+ * This class exposes an endpoint under the base path "/waiting" that allows
+ * external systems or users to trigger (fire) a waiting event based on a
+ * specific event ID.
+ */
+
+@Path("waiting")
+public class WaitingService {
+
+	/**
+	 * Fire an intermediate event based on the given event ID.
+	 * 
+	 * This endpoint can be used to resume a process or workflow that is waiting for
+	 * the specified event to occur.
+	 *
+	 * @param eventId the ID of the event to be fired
+	 */
+
+	@GET
+	@Path("/fire/{eventId}")
+	public void fireWaitingEvent(@PathParam("eventId") String eventId) {
+		IvyUtils.fireIntermediateEventByEventId(eventId);
+	}
+}
